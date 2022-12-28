2045c9e435a94bcd67430075df05a2428b33a999
Henri <henri@gotoblink.com>
2022-12-28 21:49:40 +0700

    Added product variants functionality and test

--- src/create_product_test.rs
+++ src/create_product_test.rs
@@ -1,17 +1,62 @@
-use diesel::Connection;
+use diesel::result::Error;
 use ::shoe_store::establish_connection_test;
 
 #[test]
 fn create_product_test() {
     let connection = establish_connection_test();
     connection.test_transaction::<_, Error, _>(|| {
-        let result = 
-            create_product(NewProduct {
+        create_product(NewCompleteProduct {
+            product: NewProduct {
                 name: "boots".to_string(),
                 cost: 13.23,
                 active: true
-            }, &connection);
-        assert_eq!(Ok(1), results);
+            },
+            variants: vec![
+                NewVariantValue {
+                    variant: NewVariant {
+                        name: "size".to_string()
+                    },
+                    values: vec![
+                        Some(12.to_string()),
+                        Some(14.to_string()),
+                        Some(16.to_string()),
+                        Some(18.to_string())
+                    ]
+                }
+            ]
+        }, &connection).unwrap();
+
+        assert_eq!(
+            serde_json::to_string(&list_products(&connection).unwrap()).unwrap(),
+            serde_json::to_string(&vec![
+                (
+                    Product {
+                        id: 1,
+                        name: "boots".to_string(),
+                        cost: 13.23,
+                        active: true
+                    },
+                    vec![
+                        (
+                            Some(12.to_string()),
+                            "size".to_string()
+                        ),
+                        (
+                            Some(14.to_string()),
+                            "size".to_string()
+                        ),
+                        (
+                            Some(16.to_string()),
+                            "size".to_string()
+                        ),
+                        (
+                            Some(18.to_string()),
+                            "size".to_string()
+                        )
+                    ]
+                ),
+            ]).unwrap()
+        );
 
         Ok(())
     });
